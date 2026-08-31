7740b0d390a348766f45457167df4f52b8949489
Rajkumar <rajkumarmuliki8@gmail.com>
2026-08-31 11:30:42 +0530

    Delete src directory

--- src/routes/payments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { makePayment, myPayments } = require('../controllers/paymentController');
-const { authMiddleware, roleMiddleware } = require('../middleware/auth');
-
-router.post('/', authMiddleware, roleMiddleware('participant'), makePayment);
-router.get('/mine', authMiddleware, roleMiddleware('participant'), myPayments);
-
-module.exports = router;
